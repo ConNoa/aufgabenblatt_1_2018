9aa7cdac892a55006f4d5e4ea7196f7cb2329de6
ConNoa <constantin.noack@uni-weimar.de>
2018-04-18 11:51:51 +0200

    AUfgabe1.3

---
+++ source/13.cpp
@@ -0,0 +1,22 @@
+#include  <iostream>// Ein-/Ausgabe
+
+//C++ Programme beginnen mit der Ausführung der Funktionen
+
+int main()
+{
+	int n =1;
+	int i= 1;
+
+	while(i<=20){
+		if(n%i != 0)
+		{
+			n++;
+			i = 0;
+		}
+		++i;
+	}
+	
+  	std::cout << "Die Zahl ist: ";
+  	std::cout << n << std::endl;
+  	return 0;	
+}
